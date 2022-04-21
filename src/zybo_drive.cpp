700d073c4f5542d5d195b9583a31e7515410194c
Andrew <adejonge@oakland.edu>
2022-04-21 16:05:04 -0400

    git and test added to Package.xml
    Also changed the code to write/read from GPIO memory mapped registers

--- src/zybo_drive.cpp
+++ src/zybo_drive.cpp
@@ -9,20 +9,41 @@ WHEEL_LENGTH: 0.1524m
 */
 //GAMMA is the constant relating steering wheel angle 
 //  (in our case, servo angle) to tire angle
-const double GAMMA = 700;
+const double GAMMA = 600;
 
 ros::Publisher steering_cmd;
 ros::Publisher cmd_vel;
 
-void recvTwist(const geometry_msgs::TwistConstPtr& msg){
-  double v = msg->linear.x;
-  double psi_dot = msg->angular.z;
+void timerCallback(const ros::TimerEvent& event){
+  const char * PLCommand = (const char *)0x412F0000;
+  char * ServoOutput = (char *)0x41200000;
+  double v = 5.0;
+  double psi_dot = 0.0;
+
+if(*PLCommand == 0) { //strong left
+  v = 0.0;
+  psi_dot = 0.0;
+}else if(*PLCommand == 1){ //slight left
+  v = 0.0;
+  psi_dot = 0.0;
+}else if(*PLCommand == 2){ //straight
+  v = 0.0;
+  psi_dot = 0.0;
+}else if(*PLCommand == 3){ //slight right
+  v = 0.0;
+  psi_dot = 0.0;
+}else if(*PLCommand == 4){ //strong right
+  v = 0.0;
+  psi_dot = 0.0;
+}
+  
 
   geometry_msgs::Twist vel;
   std_msgs::Float64 steering;
 
   vel.linear.x = v;
   steering.data = GAMMA*atan((WHEEL_LENGTH*psi_dot)/v);
+  *ServoOutput = 64;
 
   cmd_vel.publish(vel);
   steering_cmd.publish(steering);
@@ -36,7 +57,7 @@ int main(int argc, char** argv){
   cmd_vel = zybo_drive.advertise<geometry_msgs::Twist>("audibot/cmd_vel", 1);
   steering_cmd = zybo_drive.advertise<std_msgs::Float64>("audibot/steering_cmd", 1);
 
-  ros::Subscriber sub_twist = zybo_drive.subscribe("twist_cmd", 1, recvTwist);
+  ros::Timer marker_timer = zybo_drive.createTimer(ros::Duration(1.0/60), timerCallback);
 
   ros::spin();
 }
